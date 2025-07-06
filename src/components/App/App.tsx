d2497b06ec86bf55a190cb1f56d5060f3c4576cc
Anna_Chernobrovenko <annanyuska2404@gmail.com>
2025-07-06 17:04:19 +0200

    Deploy build

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,57 @@
 import { useState } from 'react';
-import { Toaster } from 'react-hot-toast';
-import { fetchMoviesByQuery } from '../../services/movieService';
+import toast, { Toaster } from 'react-hot-toast';
 import type { Movie } from '../../types/movie';
+import { fetchMoviesByQuery } from '../../services/movieService';
+
 import SearchBar from '../SearchBar/SearchBar';
 import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
 
+import css from './App.module.css';
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
   const handleSearch = async (query: string) => {
+    setError(false);
+    setLoading(true);
+    setMovies([]);
+
     try {
       const results = await fetchMoviesByQuery(query);
+
+      if (results.length === 0) {
+        toast('No movies found for your request.');
+      }
+
       setMovies(results);
-    } catch (error) {
-      console.error('Error fetching movies:', error);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div>
-      <SearchBar onSubmit={handleSearch} />
-      <MovieGrid movies={movies} />
+    <div className={css.app}>
       <Toaster position="top-right" />
+      <SearchBar onSubmit={handleSearch} />
+
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+
+      {!loading && !error && movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={setSelectedMovie} />
+      )}
+
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={() => setSelectedMovie(null)} />
+      )}
     </div>
   );
 }
---
+++ src/components/MovieModal/MovieModal.tsx
@@ -0,0 +1,27 @@
+
+import type { Movie } from '../../types/movie';
+
+import css from './MovieModal.module.css';
+
+type Props = {
+  movie: Movie;
+  onClose: () => void;
+};
+
+export default function MovieModal({ movie, onClose }: Props) {
+  return (
+    <div className={css.backdrop} onClick={onClose}>
+      <div className={css.modal} onClick={e => e.stopPropagation()}>
+        <button className={css.closeButton} onClick={onClose}>
+          ×
+        </button>
+        <h2>{movie.title}</h2>
+        <p>{movie.overview}</p>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+      </div>
+    </div>
+  );
+}
